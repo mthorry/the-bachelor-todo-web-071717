bf118d81dfdc243500268ca9ce7a09cc20025e57
mthorry <mthorry@gmail.com>
2017-07-12 19:12:42 -0400

    done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num,contestants|
+    if season_num == season
+      contestants.each do |key, value|
+        if key["status"] == "Winner"
+          return key["name"].split(" ").first
+        end
+      end
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num,contestants|
+    contestants.each do |key, value|
+      if key["occupation"] == occupation
+        return key["name"]
+      end
+    end
+  end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometowners = 0
+  data.each do |season_num,contestants|
+    contestants.each do |key, value|
+      if key["hometown"] == hometown
+        hometowners += 1
+      end
+    end
+  end
+  hometowners
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_num,contestants|
+    contestants.each do |key, value|
+      if key["hometown"] == hometown
+        return key["occupation"]
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  avg_age = 0
+  total = 0
+  data.each do |season_num,contestants|
+    if season_num == season
+      contestants.each do |key, value|
+      avg_age += key["age"].to_f
+      total += 1
+    end
+  end
+  end
+  (avg_age/total).round
 end
